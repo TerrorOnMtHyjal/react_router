955ee0dfa8359fb8b243632e5936779eb3ae6665
dustwise <jared.mohney@gmail.com>
2017-03-09 09:34:15 -0700

    working skeleton with emails imported

---
+++ js/components/email-app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default class EmailApp extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+
+    };
+    this.emails = {
+        inbox: {
+            0: {
+                id: 0,
+                from: "billg@microsoft.com",
+                to: "TeamWoz@Woz.org",
+                title: "Possible work opportunity",
+                content: "Dear Woz.  Fancy a job at Mister Softee?  Bill x"
+            },
+            1: {
+                id: 1,
+                from: "zuck@facebook.com",
+                to: "TeamWoz@Woz.org",
+                title: "Do you know PHP?",
+                content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+            }
+        },
+        spam: {
+            0: {
+                id: 0,
+                from: "ChEaPFl1ghTZ@hotmail.com",
+                to: "TeamWoz@Woz.org",
+                title: "WaNt CHEEp FlitZ",
+                content: "Theyre CheEp"
+            },
+            1: {
+                id: 1,
+                from: "NiKEAIRJordanZ@hotmail.com",
+                to: "TeamWoz@Woz.org",
+                title: "JorDanz For SAle",
+                content: "Theyre REELY CheEp"
+            }
+        }
+    };
+  }
+  render(){
+    return <h1>Working! Welcome to JM-Mail!</h1>
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDom from 'react-dom';
+import EmailApp from './components/email-app';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDom.render(<EmailApp/>, document.getElementById('app'));
+});
 
